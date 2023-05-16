c81f005603bfe88d0c929e6250dde2863bb78049
Gideon <kipkerring.gedion@gmail.com>
2023-05-16 00:22:18 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Update updated_at with current datetime."""
         self.updated_at = datetime.today()
         models.storage.save()
 
@@ -39,13 +39,13 @@ class BaseModel:
         Includes the key/value pair __class__ representing
         the class name of the object.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        mdict = self.__dict__.copy()
+        mdict["created_at"] = self.created_at.isoformat()
+        mdict["updated_at"] = self.updated_at.isoformat()
+        mdict["__class__"] = self.__class__.__name__
+        return mdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """Return the str representation of the BaseModel instance."""
+        clsname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
